463972823ddc36eaae7a36849f243ff2531ddd4a
Michael Monetized <michaelmonetized@gmail.com>
2024-09-05 10:39:14 -0400

    init

---
+++ app/404.tsx
@@ -0,0 +1,15 @@
+import Logo from "@/components/logo";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-stretch justify-center gap-md p-md w-full max-w-[1170px] text-center">
+        <Logo />
+        <p>{"We're currently working on this feature."}</p>
+        <p><button>{"Click Here To Try Again"}</button> - or - <Link href="/">Start Over</Link></p>
+      </div>
+    </div>
+  );
+}
+
